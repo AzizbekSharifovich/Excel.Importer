30ce321bcca042d68ad67cd9d23d3b624ed4c069
AzizbekSharifovich <azizbek0698@gmail.com>
2023-09-23 13:52:43 +0500

    BROKERS: Add Header Documentation

--- Tarteeb.Importer/Brokers/Logging/LoggingBroker.cs
+++ Tarteeb.Importer/Brokers/Logging/LoggingBroker.cs
@@ -1,6 +1,10 @@
-﻿namespace Tarteeb.Importer.Brokers.Logging
+﻿using System;
+using Tarteeb.importer.Models.Exceptions;
+
+namespace Tarteeb.Importer.Brokers.Logging;
+
 public class LoggingBroker
 {
-
+    public void LoggingError(NullClientException nullClientException) =>
+        Console.WriteLine(nullClientException.Message);
 }
-
