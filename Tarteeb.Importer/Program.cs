419e8b2e414101fbca044736a8923564b46e3097
AzizbekSharifovich <azizbek0698@gmail.com>
2023-09-26 17:46:15 +0500

    CODE RUB: Add Exception

--- Tarteeb.Importer/Program.cs
+++ Tarteeb.Importer/Program.cs
@@ -9,6 +9,7 @@ using Tarteeb.importer.Brockers.Storages;
 using Tarteeb.importer.Models.Exceptions;
 using Tarteeb.importer.Services.Clients;
 using Tarteeb.Importer.Models.Clients;
+using Tarteeb.Importer.Models.Exceptions;
 
 namespace Tarteeb.importer;
 
@@ -20,17 +21,27 @@ public class Program
         {
             using (var storageBroker = new StorageBroker())
             {
-                Client client = null;
+                var client = new Client
+                {
+                    FirstName = "Azizbek",
+                    Id = Guid.NewGuid(),
+                    Email = "something",
+                    GroupId = Guid.NewGuid(),
+                };
 
                 var clientServices = new ClientServices(storageBroker);
-                Client persistedClient = await clientServices.AddClientAsync(client);
-                Console.WriteLine(persistedClient.Id);
+                var persistedClient = await clientServices.AddClientAsync(client);
+                Console.WriteLine(persistedClient.FirstName);
 
             }
         }
         catch (NullClientException exception)
         {
             Console.WriteLine(exception.Message);
+        }
+        catch(InvalidClientException invalidClientException)
+        {
+            Console.WriteLine(invalidClientException.Message);
         }
     }
 }
