df2ffa84b069587d1e472895002babb3f5a6f808
AzizbekSharifovich <azizbek0698@gmail.com>
2023-09-26 17:41:59 +0500

    Add DateTimeBroker

---
+++ Tarteeb.Importer/Brokers/DataTimeBroker/DataTimeBroker.cs
@@ -0,0 +1,15 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//=================================
+
+using System;
+
+namespace Tarteeb.Importer.Brokers.DataTimeBroker;
+
+public class DataTimeBroker
+{
+    DateTimeOffset GetCurrentDateTimeOffset() =>
+        DateTimeOffset.UtcNow;
+}
+
