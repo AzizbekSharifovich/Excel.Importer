d83f66a40a17518c31b7b40387f556c0df0c9985
AzizbekSharifovich <azizbek0698@gmail.com>
2023-09-27 22:07:58 +0500

    BROKERS: Add LoggingBroker

--- Tarteeb.Importer/Brokers/Logging/LoggingBroker.cs
+++ Tarteeb.Importer/Brokers/Logging/LoggingBroker.cs
@@ -1,6 +1,14 @@
-﻿namespace Tarteeb.Importer.Brokers.Logging
+﻿//=================================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//=================================
+using System;
+using Tarteeb.importer.Models.Exceptions;
+
+namespace Tarteeb.Importer.Brokers.Logging;
+
 public class LoggingBroker
 {
-
+    public void LoggingError(NullClientException nullClientException) =>
+        Console.WriteLine(nullClientException.Message);
 }
-
