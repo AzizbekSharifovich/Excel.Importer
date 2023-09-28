26799dcecc4d42124fbfd9745d0ee76c863ab9d6
AzizbekSharifovich <azizbek0698@gmail.com>
2023-09-28 23:10:32 +0500

    CLIENT: ok

--- Tarteeb.Importer/Program.cs
+++ Tarteeb.Importer/Program.cs
@@ -4,12 +4,15 @@
 //===============================
 
 using System;
+using System.Collections.Generic;
+using System.Collections;
 using System.Threading.Tasks;
 using Tarteeb.importer.Brockers.Storages;
 using Tarteeb.importer.Models.Exceptions;
 using Tarteeb.importer.Services.Clients;
 using Tarteeb.Importer.Models.Clients;
 using Tarteeb.Importer.Models.Exceptions;
+using System.Linq;
 
 namespace Tarteeb.importer;
 
@@ -23,7 +26,7 @@ public class Program
             {
                 var client = new Client
                 {
-                    FirstName = "Azizbek",
+                    FirstName = "John",
                     Id = Guid.NewGuid(),
                     Email = "something",
                     GroupId = Guid.NewGuid(),
@@ -42,6 +45,15 @@ public class Program
         catch(InvalidClientException invalidClientException)
         {
             Console.WriteLine(invalidClientException.Message);
+
+            foreach (DictionaryEntry entry in invalidClientException.Data)
+            {
+                string errorSummery = ((List<string>)entry.Value)
+                    .Select((string value) => value)
+                    .Aggregate((string current, string next) => current + "," + next);
+
+                Console.WriteLine(entry.Key + "-" + errorSummery);
+            }
         }
     }
 }
