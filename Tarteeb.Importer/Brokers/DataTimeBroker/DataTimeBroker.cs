057280cad3417b2f57efc29236ce701e0479f560
AzizbekSharifovich <azizbek0698@gmail.com>
2023-10-03 14:06:07 +0500

    POC: Add Brokers

---
+++ Tarteeb.Importer/Brokers/DataTimeBroker/DataTimeBroker.cs
@@ -0,0 +1,14 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC
+// Powering True Leadership
+//=================================
+using System;
+
+namespace Tarteeb.Importer.Brokers.DataTimeBroker;
+
+public class DataTimeBroker
+{
+    public DateTimeOffset GetCurrentDateTimeOffset() =>
+        DateTimeOffset.UtcNow;
+}
+
